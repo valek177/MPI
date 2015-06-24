5db7010e198aa7bde63f74673336c17c13a94b22
Николай <@Nicl-PC2>
2015-06-24 22:12:34 +0300

    bug fixed

--- src/java/toothfairy1/Managers/UserManager.java
+++ src/java/toothfairy1/Managers/UserManager.java
@@ -32,58 +32,7 @@ public class UserManager {
     public static List<User> GetAll() throws SQLException, UnsupportedEncodingException
     {
         List<User> list = new ArrayList<User>();
-        //вот тут из базы надо брать
-        /*User t1 = new User();
-        t1.id = 1;
-        t1.login="mf";
-        t1.password="123";
-        t1.roleId=2;
-        t1.roleEntityId=1;
-
-        User t2 = new User();
-        t2.id = 2;
-        t2.login="r";
-        t2.password="123";
-        t2.roleId=4;
-        t2.roleEntityId=1;
-        list.add(t1);
-        list.add(t2);
-
-        User t3 = new User();
-        t3.id = 3;
-        t3.login="sf";
-        t3.password="123";
-        t3.roleId=1;
-        t3.roleEntityId=1;
-        
-        User t4 = new User();
-        t4.id = 4;
-        t4.login="mf2";
-        t4.password="123";
-        t4.roleId=2;
-        t4.roleEntityId=2;
-        
-        User t5 = new User();
-        t5.id = 5;
-        t5.login="mf3";
-        t5.password="123";
-        t5.roleId=2;
-        t5.roleEntityId=3;
-        
-        User t6 = new User();
-        t6.id = 6;
-        t6.login="mf4";
-        t6.password="123";
-        t6.roleId=2;
-        t6.roleEntityId=4;
-        
-        list.add(t1);
-        list.add(t2);
-        list.add(t3);
-        list.add(t4);
-        list.add(t5);
-        list.add(t6);*/
-        
+                
         Factory atata = new Factory();
         List<Users> users = atata.getInstance().getUsersDAO().getAllUsers();
         for(int i = 0; i < users.size(); ++i) {
@@ -91,6 +40,7 @@ public class UserManager {
               u1.id = users.get(i).getId();
               u1.login = (users.get(i).getLogin());
               u1.name = Helpers.convert((users.get(i).getName()));
+              u1.email = (users.get(i).getEmail());
               u1.password = (users.get(i).getPassword());
               u1.roleId = (users.get(i).getRoleId());
               u1.roleEntityId = (users.get(i).getRoleEntityId());
